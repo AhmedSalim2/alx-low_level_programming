d47312b927e13e7288cbaf2163c75946bf6dcab4
AhmedSalim2 <ahmedrama8917@gmail.com>
2022-12-13 16:44:50 -0800

    yes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int
-main(void) 
-{  
-int n;
-    
+main(void)
+{int n;
+
 for (n = 0; n < 10; n++)
-{      
-putchar(n % 10 + '0');      
-if (n != 9)	
-{	  
-putchar(',');	  
-putchar(' ');	  
-}      
-}  
+{
+putchar(n % 10 + '0');
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
-  
+
 return (0);
 }
